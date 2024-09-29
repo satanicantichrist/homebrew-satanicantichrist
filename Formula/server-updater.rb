3fb8c67fac302fc7a7100ada697b58f722009c68
satanicantichrist <burianek.jan@protonmail.com>
2024-09-29 21:42:06 +0200

    Updated server-updater to 1.0.4

--- Formula/server-updater.rb
+++ Formula/server-updater.rb
@@ -1,8 +1,8 @@
 class ServerUpdater < Formula
   desc "Small bash script for fast and easy updating of paper spigot servers."
   homepage "https://github.com/satanicantichrist/Server-Updater"
-  url "https://github.com/satanicantichrist/Server-Updater/archive/refs/tags/1.0.3.tar.gz"
-  sha256 "6cf662ce6e10b7395f8517c00975cc13f5b4e933af8784577bbd8ca695d74531"
+  url "https://github.com/satanicantichrist/Server-Updater/archive/refs/tags/1.0.4.tar.gz"
+  sha256 "eb2311e54921e89e966a4667c1ab34e34d2fb1683f4987ce1a1a8a230d6a3873"
   license ""
 
   depends_on "jq"
