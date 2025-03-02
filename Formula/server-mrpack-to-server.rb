e6e9cfb6ad2608eb570f2d6ac7de816f1290979a
satanicantichrist <burianek.jan@protonmail.com>
2025-03-02 16:04:56 +0100

    Fixed sha256 for mrpack-to-server

--- Formula/server-mrpack-to-server.rb
+++ Formula/server-mrpack-to-server.rb
@@ -2,7 +2,7 @@ class ServerMrpackToServer < Formula
   desc "Small bash script for fast and easy server generate from .mrpack file."
   homepage "https://github.com/satanicantichrist/mrpack-to-server"
   url "https://github.com/satanicantichrist/mrpack-to-server/archive/refs/tags/1.0.1.tar.gz"
-  sha256 "f73d8791fcc07eebd7414f5ed83c99cecf9e2fb5e5172c455c973ea35912a5e4"
+  sha256 "1d48d95db56b9b502411f6fd6adbc1d284191daa095c567a1c486704606d9fe8"
   license ""
 
   depends_on "jq"
