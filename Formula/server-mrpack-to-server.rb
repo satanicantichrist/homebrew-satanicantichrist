b7db6a151ec5907ec88716e695ea5ee69cdc4623
satanicantichrist <burianek.jan@protonmail.com>
2025-03-02 16:01:34 +0100

    Updated mrpack-to-server

--- Formula/server-mrpack-to-server.rb
+++ Formula/server-mrpack-to-server.rb
@@ -1,7 +1,7 @@
 class ServerMrpackToServer < Formula
   desc "Small bash script for fast and easy server generate from .mrpack file."
   homepage "https://github.com/satanicantichrist/mrpack-to-server"
-  url "https://github.com/satanicantichrist/mrpack-to-server/archive/refs/tags/1.0.0.tar.gz"
+  url "https://github.com/satanicantichrist/mrpack-to-server/archive/refs/tags/1.0.1.tar.gz"
   sha256 "f73d8791fcc07eebd7414f5ed83c99cecf9e2fb5e5172c455c973ea35912a5e4"
   license ""
 
