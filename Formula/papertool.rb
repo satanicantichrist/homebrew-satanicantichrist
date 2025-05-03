6d12be71bf42a40b9afff91a7daf69e8fe5473c8
satanicantichrist <burianek.jan@protonmail.com>
2025-05-03 13:54:13 +0200

    Fixed jg to jq in Paper-Tool

--- Formula/papertool.rb
+++ Formula/papertool.rb
@@ -5,7 +5,7 @@ class Papertool < Formula
   sha256 "3431fc96711d5cf7443b9b08fb498e82b8f0e6e2c2e33b5d1d7b929180e37898"
   license ""
 
-  depends_on ["jg", "fzf"]
+  depends_on ["jq", "fzf"]
 
   def install
     bin.install "papertool"
