5e08fdbbab97e1d9b4e0b3a7a65b6bf7d1f1afcc
satanicantichrist <burianek.jan@protonmail.com>
2025-09-14 12:18:15 +0200

    Updated Paper-tool to 1.3.0

--- Formula/papertool.rb
+++ Formula/papertool.rb
@@ -1,8 +1,8 @@
 class Papertool < Formula
   desc "Small bash script for fast and easy installation, updating of paper spigot servers."
   homepage "https://github.com/satanicantichrist/Paper-Tool"
-  url "https://github.com/satanicantichrist/Paper-Tool/archive/refs/tags/1.2.2.tar.gz"
-  sha256 "3431fc96711d5cf7443b9b08fb498e82b8f0e6e2c2e33b5d1d7b929180e37898"
+  url "https://github.com/satanicantichrist/Paper-Tool/archive/refs/tags/1.3.0.tar.gz"
+  sha256 "2fbadef40e100044f441e43c5a15e96660e9ff80b7cbf0be2f777b5551215d37"
   license ""
 
   depends_on "jq"
